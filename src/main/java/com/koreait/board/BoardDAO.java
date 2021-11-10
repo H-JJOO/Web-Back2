595819f82e68b81b2775581f014ec41ce05de377
jhyeo <icepri3535@gmail.com>
2021-11-10 21:29:41 +0900

    boardWeb2 clear

--- src/main/java/com/koreait/board/BoardDAO.java
+++ src/main/java/com/koreait/board/BoardDAO.java
@@ -90,6 +90,31 @@ public class BoardDAO {
         return null;
     }
 
+    public static int updBoard(BoardVO param) {
+        Connection con = null;
+        PreparedStatement ps = null;
+        String sql = " UPDATE t_board2 " +
+                    " SET title = ?, " +
+                    " ctnt = ?, " +
+                    " writer = ? " +
+                    " WHERE iboard = ? ";
+
+        try {
+            con = DbUtils.getCon();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, param.getTitle());
+            ps.setString(2, param.getCtnt());
+            ps.setString(3, param.getWriter());
+            ps.setInt(4, param.getIboard());
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DbUtils.close(con, ps);
+        }
+        return 0;
+    }
+
     public static int delBoard(BoardVO param) {
         Connection con = null;
         PreparedStatement ps = null;
@@ -108,5 +133,4 @@ public class BoardDAO {
         }
         return 0;
     }
-
 }
--- src/main/java/com/koreait/board/BoardModServlet.java
+++ src/main/java/com/koreait/board/BoardModServlet.java
@@ -12,11 +12,44 @@ public class BoardModServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         //TODO - 화면 나올거 작성하고 (detail과 주소빼고 동일)
+        //getParameter 로 받아오는 것은 get 방식이므로 무조건 String(문자열)로 밖에 받아올수 없다.
+        String strIboard = req.getParameter("iboard");
+        int iboard = Integer.parseInt(strIboard);
+        BoardVO param = new BoardVO();
+        param.setIboard(iboard);
+
+        BoardVO data = BoardDAO.selBoardDetail(param);
+
+        req.setAttribute("data", data);
+
+        String path = "/WEB-INF/jsp/mod.jsp";
+        req.getRequestDispatcher(path).forward(req, res);
 
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         //TODO - form 값 받을거 작성 (write와 비슷, 추가로 iboard)
+        String title = req.getParameter("title");
+        String ctnt = req.getParameter("ctnt");
+        String writer = req.getParameter("writer");
+        String strIboard = req.getParameter("iboard");
+
+        int iboard = Integer.parseInt(strIboard);
+
+        BoardVO param = new BoardVO();
+        param.setTitle(title);
+        param.setCtnt(ctnt);
+        param.setWriter(writer);
+        param.setIboard(iboard);
+
+        int result = BoardDAO.updBoard(param);
+
+        switch (result) {
+            case 1:
+                res.sendRedirect("/detail?iboard=" + strIboard);
+                break;
+        }
+
     }
 }
